1950917b8800a6c850e24717d9bceb4d9ab7898b
Rod Vagg <rod@vagg.org>
2016-07-18 11:39:44 +1000

    simplify by using jsonist

--- npm-download-counts.js
+++ npm-download-counts.js
@@ -1,5 +1,5 @@
-const hyperquest = require('hyperquest')
-    , bl         = require('bl')
+var jsonist = require('jsonist')
+
 
 function day (s) {
   if (!(s instanceof Date)) {
@@ -12,26 +12,17 @@ function day (s) {
 
 
 function downloadCounts (pkg, start, end, callback) {
-  var url        = 'https://api.npmjs.org/downloads/range/' + day(start) + ':' + day(end) + '/' + pkg 
-  hyperquest.get(url).pipe(bl(function (err, body) {
+  var url = 'https://api.npmjs.org/downloads/range/' + day(start) + ':' + day(end) + '/' + pkg 
+
+  jsonist.get(url, function (err, doc) {
     if (err)
       return callback(err)
-
-    var doc
-    try {
-      doc = JSON.parse(body)
-    } catch (e) {
-      return callback(err)
-    }
 
     if (!doc)
       return callback(new Error('no document returned'))
 
-    if (doc.error) {
-      return callback(new Error(
-          'registry error: '
-          + doc.error ))
-    }
+    if (doc.error)
+      return callback(new Error('registry error: ' + doc.error ))
 
     callback(null, doc.downloads.map(function (row) {
       return {
@@ -39,7 +30,8 @@ function downloadCounts (pkg, start, end, callback) {
         , count : row.downloads
       }
     }))
-  }))
+  })
 }
+
 
 module.exports = downloadCounts
